1eb90958833b2eca0f66c9ae2e74d635a9f3e7aa
dynamoxavier <97818464+dynamoxavier@users.noreply.github.com>
2026-03-27 22:44:37 +0800

    Change API base URL to local backend
    Updated base URL for the clothes API to use a local backend.

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clothesAPI = axios.create({
-  baseURL: "https://clothes-backend-8uyx.onrender.com/api",
+  baseURL: 'XXXXXXXXXXX:9090/api',  // Your working backend, put your ip address and then put 9090 on the end
 });
 
 export const getClothesList = () => {
